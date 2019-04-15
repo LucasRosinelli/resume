4d7634ad1b1f82efc6cfb9f79659d6b591da5e65
Lucas Rosinelli da Rocha <lucas.rosinelli@gmail.com>
2019-04-15 19:47:56 +0100

    Repository changed from Bitbucket to GitHub

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -11,7 +11,7 @@ export default class Footer extends Component {
                             <li><a target="LucasRosinelliLinkedIn" href={resumeData.contact.linkedIn}><i className="fab fa-linkedin" /></a></li>
                             <li><a target="LucasRosinelliFacebook" href={resumeData.contact.facebook}><i className="fab fa-facebook" /></a></li>
                             <li><a target="LucasRosinelliInstagram" href={resumeData.contact.instagram}><i className="fab fa-instagram" /></a></li>
-                            <li><a target="LucasRosinelliBitbucket" href={resumeData.contact.bitbucket}><i className="fab fa-bitbucket" /></a></li>
+                            <li><a target="LucasRosinelliGitHub" href={resumeData.contact.github}><i className="fab fa-github" /></a></li>
                             <li><a target="LucasRosinelliWhatsApp" href={"https://api.whatsapp.com/send?phone=" + resumeData.contact.phone.unformatted} title={"Contact me via WhatsApp: " + resumeData.contact.phone.display}><i className="fab fa-whatsapp" /></a></li>
                             <li><a href={"tel:+" + resumeData.contact.phone.unformatted} title={"Call me: " + resumeData.contact.phone.display}><i className="fa fa-phone" /></a></li>
                             <li><a href={"mailto:" + resumeData.contact.email} title={"Email me: " + resumeData.contact.email}><i className="fa fa-envelope" /></a></li>
