8d479ecddac2b689b5e16066bba7b4028fdaf3a3
Lucas Rosinelli da Rocha <lucas.rosinelli@gmail.com>
2020-04-14 18:59:08 +0100

    Resume data updated
    - Updated role and role description
    - Added new work experience at Inscale
    - Added duties and achievements
    - Added 4 new skills
    - Updated 3 skills grade

--- src/resumeData.js
+++ src/resumeData.js
@@ -4,8 +4,8 @@ let resumeData = {
     "lastName": "Rosinelli da Rocha",
     "fullName": "Lucas Rosinelli da Rocha",
     "shortName": "Lucas Rosinelli",
-    "role": "full stack developer",
-    "roleDescription": "Experienced full stack developer with solutions created focused on company needs. A team-player member who always keeps in mind that excellent interpersonal relationship is important as a high-quality product delivered on time.",
+    "role": "tech lead",
+    "roleDescription": "Experienced software developer with solutions created focused on company needs. A team-player member who always keeps in mind that excellent interpersonal relationship is important as a high-quality product delivered on time.",
     "contact": {
         "linkedIn": "https://br.linkedin.com/in/lucasrosinelli/en",
         "facebook": "https://www.facebook.com/lucas.rosinelli",
@@ -24,47 +24,86 @@ let resumeData = {
     "resume": {
         "workExperiences": [
             {
+                "company": "INSCALE", "position": "Tech lead",
+                "country": "pt", "location": "Lisbon, Portugal",
+                "from": "April 2020", "to": "Present",
+                "description": "",
+                "duties": [
+                    "Mentoring and technical leadership to a team of developers",
+                    "Develop software solutions, new user interfaces, software tools and other applications",
+                    "Review functional specifications and participate in the development of creative ideas for functionality and user interface",
+                    "Create technical design, technical specifications, documentation of developed code",
+                    "Hiring additional personnel"
+                ],
+                "achievements": []
+            },
+            {
                 "company": "Vision-box", "position": "Software engineer",
                 "country": "pt", "location": "Lisbon, Portugal",
-                "from": "March 2019", "to": "Present",
+                "from": "March 2019", "to": "April 2020",
                 "description": "",
-                "duties": [],
-                "achievements": []
+                "duties": [
+                    "Configure new and existing functionalities for Automated Border Control",
+                    "Troubleshooting acceptance and production environments behaviours",
+                    "Assure system correctness testing different devices and projects, according to system specifications and client needs",
+                    "Maintain test scripts updated based on most recent documentation approved",
+                    "Perform all tests required in different environments (Development, SQA, Acceptance and Production) and create specific and detailed reports",
+                    "Hiring additional personnel"
+                ],
+                "achievements": [
+                    "Elaborate and present technical documentation to clients",
+                    "Create structured documentation for existing processes in the team"
+                ]
             },
             {
                 "company": "NEXXYS", "position": "Full stack developer",
                 "country": "br", "location": "São Paulo, SP, Brazil",
                 "from": "September 2012", "to": "March 2019",
                 "description": "",
-                "duties": [],
-                "achievements": []
+                "duties": [
+                    "Innovative by offering eight years of experience in the full software development lifecycle – from concept through delivery of next-generation applications and customizable solutions",
+                    "85-90% of products are created using Visual Studio 2015/2017, C# (3.5 and 4.6) and ASP.NET MVC, Web API – all of them are supported by SQL Server and MongoDB databases – version controller: Git"
+                ],
+                "achievements": [
+                    "Improvements, performance, and refactoring implemented, focusing on coding readability and maintenance, scalability, and reusability",
+                    "Increased the company’s revenue by introducing a new idea in the real-time data delivery solution for our clients"
+                ]
             },
             {
                 "company": "DIGISYSTEM", "position": "Software developer",
                 "country": "br", "location": "São Paulo, SP, Brazil",
                 "from": "October 2006", "to": "August 2012",
                 "description": "",
-                "duties": [],
-                "achievements": []
+                "duties": [
+                    "Analyzed user requirements, and translated client needs into application design",
+                    "Design, code, and support software solutions that can easily be scaled depending on client needs"
+                ],
+                "achievements": [
+                    "Reduced project costs by focusing on double-check requirements and create more accurate test processes with code review"
+                ]
             },
             {
                 "company": "Politec", "position": "Programmer",
                 "country": "br", "location": "Taquaritinga, SP, Brazil",
                 "from": "September 2004", "to": "June 2006",
                 "description": "",
-                "duties": [],
+                "duties": [
+                    "Wrote code to specification in order to build two government-facing web-based applications",
+                    "Maintain product quality by carrying out reviews, performing continual tests, and analyzing feedback"
+                ],
                 "achievements": []
             }
         ],
         "skills": [
             { "name": "C#", "size": 80, "nameClass": "csharp", "sizeClass": "eigthy" },
+            { "name": ".NET Core", "size": 50, "nameClass": "netcore", "sizeClass": "fifty" },
             { "name": "ASP.NET MVC (4, 5)", "size": 70, "nameClass": "aspnetmvc", "sizeClass": "seventy" },
-            { "name": "ASP.NET Web API", "size": 70, "nameClass": "aspnetwebapi", "sizeClass": "seventy" },
+            { "name": "ASP.NET Web API", "size": 75, "nameClass": "aspnetwebapi", "sizeClass": "seventyfive" },
             { "name": "ASP.NET SignalR", "size": 60, "nameClass": "aspnetsignalr", "sizeClass": "sixty" },
             { "name": "Windows Forms", "size": 80, "nameClass": "windowsforms", "sizeClass": "eigthy" },
             { "name": "WPF", "size": 50, "nameClass": "wpf", "sizeClass": "fifty" },
             { "name": "WCF", "size": 50, "nameClass": "wcf", "sizeClass": "fifty" },
-            { "name": "REST/SOAP", "size": 60, "nameClass": "restsoap", "sizeClass": "sixty" },
+            { "name": "REST/SOAP", "size": 70, "nameClass": "restsoap", "sizeClass": "seventy" },
             { "name": "Entity Framework", "size": 60, "nameClass": "entityframework", "sizeClass": "sixty" },
             { "name": "Dapper", "size": 40, "nameClass": "dapper", "sizeClass": "forty" },
             { "name": "JavaScript", "size": 80, "nameClass": "javascript", "sizeClass": "eigthy" },
@@ -81,7 +120,10 @@ let resumeData = {
             { "name": "Ionic", "size": 60, "nameClass": "ionic", "sizeClass": "sixty" },
             { "name": "React Native", "size": 30, "nameClass": "reactnative", "sizeClass": "thirty" },
             { "name": "TFS", "size": 70, "nameClass": "tfs", "sizeClass": "seventy" },
-            { "name": "GIT", "size": 50, "nameClass": "git", "sizeClass": "fifty" }
+            { "name": "GIT", "size": 70, "nameClass": "git", "sizeClass": "seventy" },
+            { "name": "JIRA", "size": 70, "nameClass": "git", "sizeClass": "seventy" },
+            { "name": "Jenkins", "size": 40, "nameClass": "git", "sizeClass": "forty" },
+            { "name": "Confluence", "size": 70, "nameClass": "git", "sizeClass": "seventy" }
         ]
     },
 }
